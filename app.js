3d7f3466c9927ba345949abbe700c5f1f8de94db
Parth Yadav <yadavparth22@gmail.com>
2020-12-23 15:22:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var btntranslate=document.querySelector("#btn-translate");
+
+var txtinput=document.querySelector("#txt-input")
+
+var outputdiv=document.querySelector("#output")
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something wrong")
+}
+
+function clickHandler() {
+    var inputText = txtinput.value;   //Taking Input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputdiv.innerText = translatedText; // output
+           })
+        .catch(errorHandler)
+};
+
+btntranslate.addEventListener("click",clickHandler)
